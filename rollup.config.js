510ab18ab7690d0327a12e72a78ed16583e98db1
Chris Baker <mail.chris.baker@gmail.com>
2025-04-11 07:55:19 -0400

    chore: clean slate

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from "@rollup/plugin-typescript";
+
+export default [
+  createConfiguration("cjs", "cjs"),
+  createConfiguration("es", "mjs"),
+];
+
+function createConfiguration(format, extension) {
+  return {
+    input: "src/index.ts",
+    output: {
+      format,
+      dir: "dist",
+      entryFileNames: `[name].${extension}`,
+      chunkFileNames: `[name]-[hash].${extension}`,
+    },
+    plugins: [typescript()],
+  };
+}
